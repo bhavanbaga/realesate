f6a4e4149130ad000ab822d48f805ce5049d4758
bhavanbaga <130345674+bhavanbaga@users.noreply.github.com>
2023-04-27 12:58:43 +0530

    Add files via upload

---
+++ real.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE `contacts` (
+  `cid` int(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`cid`, `name`, `email`, `phone`, `subject`, `message`) VALUES
+(7, 'codeastro', 'asda@asd.com', '8888885454', 'codeastro.com', 'asdasdasd');
